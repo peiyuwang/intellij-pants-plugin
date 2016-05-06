18b5cf438f6b78204420746a78f7ba78bc190a86
Peiyu Wang <peiyu@twitter.com>
2016-05-06 08:28:38 -0700

    Fix script

--- scripts/jstack.sh
+++ scripts/jstack.sh
@@ -11,16 +11,14 @@ i="1"
 
 while [ ${i} -le ${NUM_SAMPLES} ]
 do
-	if [ ! -z ${PID} ]; then
-		echo "============================================================================="
-		echo "Sampling ps for pants ConsoleRunner process (${i}/${NUM_SAMPLES})"
-		echo "============================================================================="
-		echo
-		echo -n "Total NGServer: "
-        ps -ef|grep com.martiansoftware.nailgun.NGServer |grep -v grep|wc -l
-        echo
-        ps -ef|grep com.martiansoftware.nailgun.NGServer |grep -v gre
-	fi
+    echo "============================================================================="
+	echo "Sampling ps for pants ConsoleRunner process (${i}/${NUM_SAMPLES})"
+	echo "============================================================================="
+	echo
+	echo -n "Total NGServer: "
+    ps -ef|grep com.martiansoftware.nailgun.NGServer |grep -v grep|wc -l
+    echo
+    ps -ef|grep com.martiansoftware.nailgun.NGServer |grep -v gre
 
 	sleep ${SLEEP_INTERNAL_SECONDS}
 	i=$[${i}+1]
